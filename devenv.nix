9f89a313b062162248f7adb82c32364e82de8b34
Peter Esselius <peter.esselius@pagero.com>
2023-05-15 11:19:08 +0200

    ozone

--- devenv.nix
+++ devenv.nix
@@ -3,5 +3,7 @@
 {
   packages = [ pkgs.nickel pkgs.dyff ];
 
-  scripts.dyff-test.exec = "dyff between -b <(nickel export --format yaml -f scm-service.ncl) targetManifests/scm-service.yaml";
+  scripts.dyff-test-svc.exec = "dyff between -b <(nickel export --format yaml -f scm-service.ncl) targetManifests/scm-service.yaml";
+  scripts.dyff-test-sts.exec = "dyff between -b <(nickel export --format yaml -f scm-sts.ncl) targetManifests/scm-statefulset.yaml";
+  scripts.test-ozone.exec = "nickel export --format raw -f ozone.ncl";
 }
